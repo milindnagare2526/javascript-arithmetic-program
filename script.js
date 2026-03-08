304944010e0a03def100ef23601922463f363095
milindnagare2526 <milindnagare2526@ternaengg.ac.in>
2026-03-08 19:18:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+// Arithmetic Operations
+let a = 10;
+let b = 5;
+
+console.log("Arithmetic Operations:");
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+
+
+// Conditional Statement
+let number = 7;
+
+console.log("\nConditional Statement:");
+
+if (number % 2 === 0) {
+    console.log(number + " is Even");
+} else {
+    console.log(number + " is Odd");
+}
+
+
+// Loop Example
+console.log("\nLoop Example (Printing numbers 1 to 5):");
+
+for (let i = 1; i <= 5; i++) {
+    console.log("Number:", i);
+}
